17d803546ecb980bc8abd9b9c977d01caa40bcee
ARTHUR FINCHAM <arthurfincham@me.com>
2021-07-11 12:46:27 +0100

    back end working

---
+++ README.md
@@ -0,0 +1,25 @@
+<h2>TITLE</h2>
+
+![](https://img.shields.io/github/last-commit/arthurfincham/random_reviews)
+![](https://img.shields.io/github/languages/count/arthurfincham/random_reviews)
+![](https://img.shields.io/github/languages/code-size/arthurfincham/random_reviews)
+
+<h3>Description</h3>
+
+A random review card generator.
+
+<h3>Features</h3>
+<ul>
+  <li>Each profile image is randomly AI generated from [thispersondoesnotexist.com](https://thispersondoesnotexist.com)</li>
+  <li>Reviewer's rating is randomly generated. Stars are filled accordingly. Help from tutorial by [George Marksoukos](https://webdesign.tutsplus.com/tutorials/a-simple-javascript-technique-for-filling-star-ratings--cms-29450).</li>
+  <li>Stars are from an icon library called [Font Awesome](http://fontawesome.com)</li>
+</ul>
+
+<h3>Problems</h3>
+<ul>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+
+<h3>:link: Links</h3>
---
+++ main.js
@@ -0,0 +1,34 @@
+const rating = (Math.random() * 5) + 1;
+const starTotal = 5;
+const starPercentage = (rating / starTotal) * 100;
+const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+document.querySelector(".stars-inner").style.width = starPercentageRounded;
+
+const name_num = Math.floor(Math.random() * (4 + 1));
+const names = [
+  "Alex Townsend",
+  "Billie Grealish",
+  "Cameron Brown",
+  "Fran Rossi",
+  "Bobbie James"
+]
+document.getElementById("name").innerHTML = names[name_num]
+
+const location_num = Math.floor(Math.random() * (4 + 1));
+const locations = [
+  "Mark's Garage, Southwark",
+  "Zoe's Cafe, Pimlico",
+  "Le Petit Croissant, Chelsea",
+  "Starlight Sports, West End",
+  "Fiona's Fish Shop, Archway"
+]
+document.getElementById("palace").innerHTML = locations[location_num]
+
+const review_num = Math.floor(Math.random() * (3 + 1));
+const reviews = [
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!",
+  "Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!",
+  "Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!",
+  "Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto nihil explicabo perferendis quos provident delectus ducimus necessitatibus reiciendis optio tempora unde earum doloremque commodi laudantium ad nulla vel odio?"
+]
+document.getElementById("review").innerHTML = reviews[review_num]
